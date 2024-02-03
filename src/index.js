067afb5eee20f5fe5e5cfb83693193aae1eafc2a
Partha Bhowmik <iamparthabhowmik@gmail.com>
2024-02-03 18:50:59 +0530

    learning

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 
-connectDB();
+connectDB()
+.then( () => {
+    app.on("error", (error)=>{
+        console.log("error: ",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`The server is listening at port: ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.log("MongoDB connection failed!", err)
+})
 
 
 
